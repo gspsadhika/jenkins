ca3682227ffb8cf69741c12b05fd09c036378a96
gspsadhika <59390598+gspsadhika@users.noreply.github.com>
2020-03-10 12:16:19 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+public class EvenOdd {
+public static void main(String args[])
+{
+int a = Integer.parseInt(args[0]);
+System.out.println("Enter any number");
+if(a%2==0) {
+   System.out.println("The given number is even");
+}
+else {
+System.out.println("The given number is odd");
+}
+
+}
+}
